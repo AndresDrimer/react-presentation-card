bb5428f326e46aec9441f9f96e316e3e77ce49d4
AndresDrimer <andresdrimer@hotmail.com>
2023-01-30 12:35:30 -0300

    type correction

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const data = {
     portfolioSrc: "https://drive.google.com/file/d/1TcSLUb7TKj-YSq09NgJraFFqmOhnWrtd/view",
     email: "andresdrimer@hotmail.com",
     linkedin: "https://www.linkedin.com/in/andres-drimer/",
-    aboutContent: "I am a Frontend Developer / UX Desginer .+3years experience: CSS, HTML, Javascript, Vue.js, React, Figma, Photoshop, Illustrator, Premiere, CMS e-commerce projects. I try to keep up with security and best practices, and am always looking for new things to learn.",
+    aboutContent: "I am a Frontend Developer / UX Designer .+3years experience: CSS, HTML, Javascript, Vue.js, React, Figma, Photoshop, Illustrator, Premiere, CMS e-commerce projects. I try to keep up with security and best practices, and am always looking for new things to learn.",
     interestContent: "Reading, music, entrepreneurship ",
     github: "https://github.com/AndresDrimer"
   };
