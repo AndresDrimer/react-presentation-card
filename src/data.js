607484625d8c2d7bb4cbc694ad8100af835ae75e
AndresDrimer <andresdrimer@hotmail.com>
2023-01-30 12:23:52 -0300

    ux

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 const data = {
     name: "Andres Drimer",
-    position: "Frontend Developer",
+    position: "Frontend Developer / UX designer",
     portfolioSrc: "https://drive.google.com/file/d/1TcSLUb7TKj-YSq09NgJraFFqmOhnWrtd/view",
     email: "andresdrimer@hotmail.com",
     linkedin: "https://www.linkedin.com/in/andres-drimer/",
-    aboutContent: "I am a frontend developer.+3years experience: CSS, HTML, Javascript, Vue.js, React. I try to keep up with security and best practices, and am always looking for new things to learn.",
-    interestContent: "Reading, music, entrepeneurship ",
+    aboutContent: "I am a Frontend Developer / UX Desginer .+3years experience: CSS, HTML, Javascript, Vue.js, React, Figma, Photoshop, Illustrator, Premiere, CMS e-commerce projects. I try to keep up with security and best practices, and am always looking for new things to learn.",
+    interestContent: "Reading, music, entrepreneurship ",
     github: "https://github.com/AndresDrimer"
   };
   
