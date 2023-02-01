79897d8b83e2ab3f96a539bad82c17cad48d15df
AndresDrimer <andresdrimer@hotmail.com>
2023-02-01 11:07:28 -0300

    branch change

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,9 +1,10 @@
+import data from '../data'
 export default function Name(){
     return(
         <div className="name-container">
         <h1 className="--name-name">Andrés Drimer</h1>
         <h3 className="--name-position"> Frontend Developer</h3>
-        <p className="--name-portfolio"> <a href="https://drive.google.com/file/d/1TcSLUb7TKj-YSq09NgJraFFqmOhnWrtd/view">view portfolio</a> </p>
+        <p className="--name-portfolio"> <a href={data.portfolioSrc}>portfolio</a> / <a href={data.resume}> resume</a></p>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,14 @@
+const data = {
+    name: "Andres Drimer",
+    position: "Frontend Developer",
+    portfolioSrc: "https://portfolio-ad-2022.netlify.app/",
+    email: "andresdrimer@hotmail.com",
+    linkedin: "https://www.linkedin.com/in/andres-drimer/",
+    aboutContent: "I am a Frontend Developer.+3years experience: CSS, HTML, Javascript, React, Vue.js, , Figma, Photoshop, Illustrator, Premiere, CMS e-commerce projects. I try to keep up with security and best practices, and am always looking for new things to learn.",
+    interestContent: "Reading, music, entrepreneurship ",
+    github: "https://github.com/AndresDrimer",
+    resume:"https://drive.google.com/file/d/1TcSLUb7TKj-YSq09NgJraFFqmOhnWrtd/view?usp=sharing"
+  };
+  
+
+  export default data
